d4a8bd25b10f3a0dda16e114d5225fb2082ba8ce
kotama7 <kotamatakanori2@gmail.com>
2024-10-22 18:12:01 +0900

    add:firebase database

--- app/src/lib/database/database.ts
+++ app/src/lib/database/database.ts
@@ -1,14 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyAg9bXwl_ZFJu2FuOSYzEnFJ6iSn7fA2EY",
   authDomain: "jphacks-4a068.firebaseapp.com",
+  databaseURL:
+    "https://jphacks-4a068-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jphacks-4a068",
   storageBucket: "jphacks-4a068.appspot.com",
   messagingSenderId: "738330529868",
@@ -16,6 +19,8 @@ const firebaseConfig = {
   measurementId: "G-BPDFN4B5DH",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+
+export default db;
