fd15d6fd9d7953437a11f6d88559c0bb830ca346
michi0008 <michi322418@gmail.com>
2024-10-26 18:39:05 +0900

    dev: ユーザ情報からインターンシップ情報を取得する関数群を作成

--- app/src/lib/function/dao/writeDataTable.ts
+++ app/src/lib/function/dao/writeDataTable.ts
@@ -1,11 +1,13 @@
 import db from "@/lib/database/database";
 import DataTable from "@/lib/class/DataTable";
 import { set, ref } from "@firebase/database";
-import objectToJson from "@/lib/function/dataTableToJson";
+import dataTableToJson from "@/lib/function/dataTableToJson";
 
 // ユーザ情報をデータベースに追加する関数
-export default async function writeSchedule(data_table: DataTable) {
-  const json = JSON.parse(objectToJson(data_table));
+export default async function writeDataTable(
+  data_table: DataTable
+): Promise<void> {
+  const json = JSON.parse(dataTableToJson(data_table));
   // データベースにユーザ情報を追加
   try {
     await set(ref(db, `users/${data_table.getUserId()}`), {
